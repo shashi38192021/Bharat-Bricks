aa15aaca484cdfddc54817305c7c74e032a001d0
shashi38192021 <shashi38192021@gmail.com>
2026-06-29 13:56:28 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -34,8 +34,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.listen(3000, () => {
-  console.log("Server is runnig on port 3000!!!");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 app.use("/api/user", userRouter);
